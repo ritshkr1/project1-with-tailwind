00ef4f3840ad46505bd69ebd3eaab5b6c3714960
Ritesh Kumar <44864954+ritshkr1@users.noreply.github.com>
2023-06-05 18:11:56 +0530

    made new changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+import { CreateaccountComponent } from './createaccount/createaccount.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SigninComponent },
+  { path: 'createaccount', component: CreateaccountComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { CreateaccountComponent } from './createaccount/createaccount.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    CreateaccountComponent
   ],
   imports: [
     BrowserModule,
